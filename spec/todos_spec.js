101b7fada3a14cd6b0411db8ab400c8c5a9f658a
Jacob Morris <jmorris@pivotal.io>
2014-12-16 16:47:25 -0700

    wip

--- spec/todos_spec.js
+++ spec/todos_spec.js
@@ -1,5 +1,3 @@
-document.write('<div id="ember-testing-container"></div>');
-Todos.rootElement = '#ember-testing-container';
 Todos.setupForTesting();
 Todos.injectTestHelpers();
 
@@ -13,12 +11,25 @@ beforeEach(function(){
 });
 
 describe("TodosMVC", function(){
-  it('shows the application title', function(done) {
+  it('shows the application title', function(wait) {
     visit('/');
-    done();
+    wait();
 
     // Wait for asynchronous helpers above to complete
     actualTitle = find('#todoapp header h1').text()
     expect(actualTitle).toEqual('todos');
   });
 })
+
+// describe("Creating a todo", function(){
+//   it('Adds the todo item to the view', function(wait){
+//     visit('/');
+//     wait();
+//
+//     fillIn('#new-todo', 'My new todo')
+//     keyEvent('#new-todo', 'keyup', 13)
+//     wait()
+//
+//     var list = find('#todo-list')
+//   });
+// });
